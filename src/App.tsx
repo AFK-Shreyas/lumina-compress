30cf30c8871aa68d695bbdc90c16b5ff8c95a6da
AFK-Shreyas <shreyasjoshi2004@gmail.com>
2026-05-13 17:11:12 +0530

    Update App.tsx
    Updated app.tsx for 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotifyProvider } from './context/NotifyContext'
 import { ThemeProvider } from './context/ThemeProvider'
 import { MainLayout } from './components/layout/MainLayout'
 import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
 
 const About = lazy(() => import('./pages/About').then((m) => ({ default: m.About })))
 const Privacy = lazy(() => import('./pages/Privacy').then((m) => ({ default: m.Privacy })))
@@ -47,22 +48,24 @@ export default function App() {
             }
           >
             <Routes>
-            <Route element={<MainLayout />}>
-              <Route index element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="privacy" element={<Privacy />} />
-              <Route path="terms" element={<Terms />} />
-              <Route path="resize-image" element={<ResizeImagePage />} />
-              <Route path="crop-image" element={<CropImagePage />} />
-              <Route path="compress-image-to-20kb" element={<Compress20KbPage />} />
-              <Route path="compress-image-to-50kb" element={<Compress50KbPage />} />
-              <Route path="compress-image-to-100kb" element={<Compress100KbPage />} />
-              <Route path="compress-image-to-200kb" element={<Compress200KbPage />} />
-              <Route path="passport-photo-maker" element={<PassportPhotoPage />} />
-              <Route path="instagram-image-resizer" element={<InstagramImageResizerPage />} />
-              <Route path="youtube-thumbnail-resizer" element={<YoutubeThumbnailResizerPage />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Route>
+              <Route element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="privacy" element={<Privacy />} />
+                <Route path="terms" element={<Terms />} />
+                <Route path="resize-image" element={<ResizeImagePage />} />
+                <Route path="crop-image" element={<CropImagePage />} />
+                <Route path="compress-image-to-20kb" element={<Compress20KbPage />} />
+                <Route path="compress-image-to-50kb" element={<Compress50KbPage />} />
+                <Route path="compress-image-to-100kb" element={<Compress100KbPage />} />
+                <Route path="compress-image-to-200kb" element={<Compress200KbPage />} />
+                <Route path="passport-photo-maker" element={<PassportPhotoPage />} />
+                <Route path="instagram-image-resizer" element={<InstagramImageResizerPage />} />
+                <Route path="youtube-thumbnail-resizer" element={<YoutubeThumbnailResizerPage />} />
+              </Route>
+
+              {/* 404 - outside MainLayout so it has its own full-page design */}
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </Suspense>
         </BrowserRouter>
